054893188f2176f9bea6de0726f6ef5f421be143
OwenPotent <sheanowen3@gmail.com>
2021-03-19 23:17:21 +0800

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,10 @@
 module.exports = {
     aliases: 's',
-    category: 'F'
+    category: 'Fun',
+    minArgs: 1,
+    maxArgs: -1,
+    syntaxError: "Please state a message you wish to ask the bot to send!",
+    callback: ({ message }) => {
+        message.channel.send(message)
+    }
 }
