479826d57836a5df80fa3f0f47409a37687f31b3
OwenPotent <sheanowen3@gmail.com>
2021-03-19 23:58:14 +0800

    Fixing ban.js tomorrow

--- commands/ban.js
+++ commands/ban.js
@@ -1,17 +1,9 @@
+const Discord = require('discord.js')
+
 module.exports = {
     category: "Moderation",
     expectedArgs: "<userID/Mention>",
-    callback: async ({ message, args }) => {
-        if (!message.member.hasPermission('BAN_MEMBERS')) {
-            return message.channel.send(`You are unable to ban members!`)
-        }
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-
-        try {
-            await member.ban();
-            await message.channel.send(`${member} has been banned!`)
-        } catch (e) {
-            return message.channel.send(`My apologies, it seems like this user is not in the server..`)
-        }
+    callback: async ({ client, message, args }) => {
+        message.reply("Nothing to see yet!")
     }
 }
