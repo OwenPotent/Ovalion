58e84579fd6e382b2495bad340fe20dd0ab79951
OwenPotent <sheanowen3@gmail.com>
2021-03-19 23:19:21 +0800

    Fix issue with say command

--- commands/say.js
+++ commands/say.js
@@ -5,6 +5,9 @@ module.exports = {
     maxArgs: -1,
     syntaxError: "Please state a message you wish to ask the bot to send!",
     callback: ({ message }) => {
+    
+        if (message.author.bot) return;
+        
         message.channel.send(message)
     }
 }
